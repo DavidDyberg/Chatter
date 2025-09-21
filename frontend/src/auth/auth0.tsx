61d0bf637d4bd3533583b292684d2e025fde55b4
David Dyberg <david.dyberg@gmail.com>
2025-09-21 18:33:52 +0200

    feat: add useRefreshTokens

--- frontend/src/auth/auth0.tsx
+++ frontend/src/auth/auth0.tsx
@@ -19,6 +19,8 @@ export function Auth0Wrapper({ children }: { children: React.ReactNode }) {
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
+      cacheLocation="localstorage"
+      useRefreshTokens={true}
     >
       <Auth0ContextProvider>{children}</Auth0ContextProvider>
     </Auth0Provider>
