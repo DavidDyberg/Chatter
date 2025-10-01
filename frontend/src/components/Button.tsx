cce916833b51afcbc608a7d98656c47fb451e4d1
David Dyberg <david.dyberg@gmail.com>
2025-10-01 15:22:14 +0200

    feat: add button component

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,28 @@
+import { cn } from '@/utils/classnames'
+
+type ButtonProps = {
+  label: string
+  variant: 'Primary' | 'Seacondary'
+  className?: string
+  OnClick?: () => void
+}
+
+export const ButtonComponen: React.FC<ButtonProps> = ({
+  label,
+  variant,
+  OnClick,
+  className,
+}) => {
+  return (
+    <button
+      className={cn(
+        `p-3 pl-6 pr-6 rounded-3xl font-bold cursor-pointer hover:opacity-90
+        ${variant === 'Primary' ? 'bg-purple-dark' : 'bg-white text-primary-black'}`,
+        className,
+      )}
+      onClick={OnClick}
+    >
+      {label}
+    </button>
+  )
+}
