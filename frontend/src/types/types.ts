ac84cfdd22a8a91c4f89463f3015ba5bf284c882
David Dyberg <david.dyberg@gmail.com>
2025-10-21 09:23:16 +0200

    feat: add types

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,40 @@
+export type User = {
+  id: string
+  userName: string
+  email: string
+  bio?: string
+  profileImage?: string
+  profileImageId?: string
+  profileBanner?: string
+  profileBannerId?: string
+  role: 'USER' | 'ADMIN'
+  createdAt?: string
+  updatedAt?: string
+}
+
+export type Post = {
+  id: string
+  content: string
+  image?: string
+  imageId?: string
+  createdAt: string
+  updatedAt: string
+  user_id: string
+  _count: Count
+  comments: Comment[]
+}
+
+export type Comment = {
+  id: string
+  content: string
+  createdAt: string
+  updatedAt: string
+  user_id: string
+  post_id: string
+  user: User
+}
+
+export type Count = {
+  likes: number
+  comments: number
+}
