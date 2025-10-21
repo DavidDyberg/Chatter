6bff9c431ef2e31bdded6b54e4f89a7fc28b64bb
David Dyberg <david.dyberg@gmail.com>
2025-10-21 10:46:39 +0200

    feat: include user data in posts

--- frontend/src/api-routes/posts.ts
+++ frontend/src/api-routes/posts.ts
@@ -2,6 +2,8 @@ import axios from 'axios'
 import type { Post } from '@/types/types'
 
 export const fetchPosts = async () => {
-  const { data } = await axios.get<Post[]>('http://localhost:8080/api/post')
+  const { data } = await axios.get<Post[]>(
+    'https://chatter-r8i2.onrender.com/api/post',
+  )
   return data
 }
