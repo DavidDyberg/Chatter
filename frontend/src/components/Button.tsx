5f5df1dde5188382c6843f98b3e5a1830002d50a
David Dyberg <david.dyberg@gmail.com>
2025-10-24 16:05:43 +0200

    feat: configured button component

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,28 +1,38 @@
 import { cn } from '@/utils/classnames'
+import { Loader2 } from 'lucide-react'
 
 type ButtonProps = {
   label: string
-  variant: 'Primary' | 'Seacondary'
+  variant: 'Primary' | 'Seacondary' | 'Delete'
   className?: string
   OnClick?: () => void
+  isLoading?: boolean
+  disabled?: boolean
 }
 
 export const ButtonComponent: React.FC<ButtonProps> = ({
   label,
   variant,
   OnClick,
+  isLoading = false,
+  disabled = false,
   className,
 }) => {
   return (
     <button
+      disabled={disabled || isLoading}
       className={cn(
         `p-3 pl-6 pr-6 rounded-3xl font-bold cursor-pointer hover:opacity-90
-        ${variant === 'Primary' ? 'bg-purple-dark' : 'bg-white text-primary-black'}`,
+        ${disabled ? 'opacity-60 cursor-default hover:opacity-60' : ''}
+        ${variant === 'Primary' ? 'bg-purple-dark' : variant === 'Seacondary' ? 'bg-white text-primary-black' : 'border border-red-600 text-red-600 hover:opacity-80'}`,
         className,
       )}
       onClick={OnClick}
     >
-      {label}
+      <div className="flex justify-center gap-2 items-center">
+        <p>{label}</p>
+        {isLoading && <Loader2 className="animate-spin" size={16} />}
+      </div>
     </button>
   )
 }
