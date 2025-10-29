7c8f18beb20a98763a6502c7cde50bc6409a0de3
David Dyberg <david.dyberg@gmail.com>
2025-10-29 10:58:11 +0100

    debugg

--- frontend/src/components/FileUploader.tsx
+++ frontend/src/components/FileUploader.tsx
@@ -12,7 +12,7 @@ type FileUploaderProps = {
   }) => React.ReactNode
 }
 
-const MAX_FILE_SIZE = 2 * 1024 * 1024
+const MAX_FILE_SIZE = 5 * 1024 * 1024
 
 export default function FileUploader({
   defaultImage,
@@ -48,7 +48,7 @@ export default function FileUploader({
 
     if (isHeicOrHeif) {
       try {
-        toast.loading('Converting photo...')
+        alert(`fileType:${file.type}, fileSize:${file.size}`)
 
         const converted = await heic2any({
           blob: file,
